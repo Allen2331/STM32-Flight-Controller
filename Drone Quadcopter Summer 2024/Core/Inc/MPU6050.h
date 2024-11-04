3c5f03b65b9c8ec5f2a4e4aef1f55c563c554020
Allen <anguyen2331@gmail.com>
2024-11-03 18:39:20 -0800

    added mpu6050

---
+++ Drone Quadcopter Summer 2024/Core/Inc/MPU6050.h
@@ -0,0 +1,16 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: Oct 27, 2024
+ *      Author: Allen
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+#include "stm32f4xx_hal.h" // Used for I2C
+
+#define MPU6050_I2C_ADD (0x68 << 1) // AD0 pin connected to GND
+
+
+#endif /* INC_MPU6050_H_ */
