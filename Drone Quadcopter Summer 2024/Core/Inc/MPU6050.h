c6f652a521942fd124cf20e18af8d5e153674e8c
Allen <anguyen2331@gmail.com>
2024-11-04 08:07:41 -0800

    register address

--- Drone Quadcopter Summer 2024/Core/Inc/MPU6050.h
+++ Drone Quadcopter Summer 2024/Core/Inc/MPU6050.h
@@ -8,9 +8,25 @@
 #ifndef INC_MPU6050_H_
 #define INC_MPU6050_H_
 
-#include "stm32f4xx_hal.h" // Used for I2C
+#include "stm32f4xx_hal.h" //Used for I2C
+#include <cstdint>
 
-#define MPU6050_I2C_ADD (0x68 << 1) // AD0 pin connected to GND
+//Accelerometer Register Read Addresses 
+#define ACCEL_XOUT_H 0X3B // H = First 8 bits
+#define ACCEL_XOUT_L 0x3C // L = Last 8 bits
+#define ACCEL_YOUT_H 0x3D 
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
 
+//Gyroscope Register Read Addresses
+#define GYRO_XOUT_H = 0x43
+#define GYRO_XOUT_L = 0x44
+#define GYRO_YOUT_H = 0x45
+#define GYRO_YOUT_L = 0x46
+#define GYRO_ZOUT_H = 0x47
+#define GYRO_ZOUT_L = 0x48
+
+#define MPU6050_I2C_ADDR (0x68 << 1) // AD0 pin connected to GND, may NOT need to left shift
 
 #endif /* INC_MPU6050_H_ */
